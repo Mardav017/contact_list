d5f6a0688190b728d05c4ff3a4d0dbc2342ad556
Mardav <mardav17jp@gmail.com>
2024-07-19 12:35:59 +0530

    path updated

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-// require("dotenv").config();
+require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
+const path = require("path");
 
 const userRoutes = require("./routes/userRoutes");
 const contactRoutes = require("./routes/contactRoutes");
@@ -14,6 +15,9 @@ const PORT = process.env.PORT;
 
 // Middleware
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -21,7 +25,6 @@ app.use(express.urlencoded({ extended: true }));
 // Database connection
 // const dbURI = "mongodb://localhost:27017/demo";
 const dbURI = process.env.dbURI;
-console.log('DB_URI:', process.env.DB_URI);
 
 // Session 
 app.use(
